523c611c1913a1757362e8268e24d3de414a33f4
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2026-07-02 16:36:21 -0600

    update fileName route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import Header from './components/header';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
 import './App.css'
+
 
 function App() {
 
   return (
     <>
       <Header />
+      <HomePage />
       <Footer />
     </>
   )
