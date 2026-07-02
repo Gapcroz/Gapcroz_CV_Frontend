ba6f00100bb5e9edf77a32c888c11b84ce9a2dd9
Gibrán Pérez <109172763+Gapcroz@users.noreply.github.com>
2026-07-02 16:14:44 -0600

    Initial CV page

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + TypeScript + Vite
+
+
+
+# REFERENCIAS DE INSTALADORES
+1. https://tailwindcss.com/docs/installation/using-vite
---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+function App() {
+
+  return (
+    <>
+      <section>
+        <div>
+          <h1 className='text-blue-700 text-3xl'>GIBRÁN PÉREZ</h1>
+          <h2>WEB DEVELOPER</h2>
+          <p>
+            I am a web developer with experience in building responsive and user-friendly web applications.
+            I am passionate about creating intuitive user interfaces and delivering high-quality code.
+            Welcome.
+          </p>
+        </div>
+
+      </section>
+    </>
+  )
+}
+
+export default App
